bdb847db715a885b8fb3b4febb231131b8ec8489
harsit786 <99singhharshit@gmail.com>
2020-09-09 10:46:16 +0530

    Second Project Commit

---
+++ student/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2019 Campus Portal| By : <a href="http://nith.ac.in/" target="_blank">NIT HAMIRPUR</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
---
+++ student/header.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+?>
+
+    <!-- HEADER END-->
+    <div class="navbar navbar-inverse set-radius-zero">
+       
+            <div class="toplog">
+			<img id="imglog" src="nith.jpeg">
+			<div id="toptext">
+				<h1>NATIONAL INSTITUTE OF TECHNOLOGY</h1>
+				<h3 style="margin-left: 100px">
+				CAMPUS PORTAL SYSTEM
+				</h3>
+			</div>	
+	    </div>
+
+           
+          </div>
